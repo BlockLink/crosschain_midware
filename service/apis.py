44e02380cb00109b48fbd02a5c4a2b166812443c
Sun Yu <sunyu@new4g.cn>
2017-06-30 13:25:39 +0800

    增加归账和创建账户的桩

--- service/apis.py
+++ service/apis.py
@@ -8,3 +8,26 @@ def index():
     logger.info('App.index')
     return 'Welcome to Flask JSON-RPC'
 
+
+
+@jsonrpc.method('Zchain.Create.Address(coin=String)')
+def zchain_create_address(coin):
+    logger.info('Create_address coin: %s'%(coin))
+    if coin == 'eth':
+        address = ''
+        return {'coin':coin,'address':address}
+    elif coin == 'btc':
+        address = ''
+        return {'coin':coin,'address':address}
+
+
+
+@jsonrpc.method('Zchain.Collection.Amount(coin=String,address=String,amount=double)')
+def zchain_collection_amount(coin,address,amount):
+    logger.info('Create_address coin: %s'%(coin))
+    if coin == 'eth':
+        return {'coin':coin,'result':True}
+    elif coin == 'btc':
+        return {'coin':coin,'result':True}
+
+
