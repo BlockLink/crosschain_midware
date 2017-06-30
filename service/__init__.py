7d09467f322dfe0f7a64da2bbddadfc3d3a03a23
zhangzt <28299209@qq.com>
2017-06-30 13:08:06 +0800

    Remove flask_admin.

--- service/__init__.py
+++ service/__init__.py
@@ -5,14 +5,12 @@ from config import logger
 from flask import Flask
 from config import config
 from flask_jsonrpc import JSONRPC
-from flask_admin import Admin
 
 
 logger.info('hello')
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'))
 
-admin = Admin(app, name=u'区块链查询平台', template_mode='bootstrap3')
 jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
 
 app.config.from_object(config['development'])   # 载入配置文件
