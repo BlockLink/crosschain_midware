2d1f4724a5d15eb429cec6c278fc86fa35d72c33
tedmosby <530623363@qq.com>
2017-07-07 11:06:42 +0800

    btc_client_add

--- utils/btc_utils.py
+++ utils/btc_utils.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
-
-from __future__ import print_function
-from . import rpc
 import requests
-import json
-from config import config
-from service import db
-# from service import models
+from base64 import encodestring
+
 def btc_request(method,args):
-    btc_conn = rpc.create_btc_rpc_client()
-    return btc_conn.request(method,args)
-    pass
+    url = "http://127.0.0.1:60011/"
+    user = 'a'
+    passwd = 'b'
+    basestr = encodestring('%s:%s' % (user,passwd))[:-1]
+    args_str = ''.join(['[', ','.join(['"' + str(arg) + '"' for arg in args]), ']'])
+    payload =  "{\r\n \"id\": 1,\r\n \"method\": \"%s\",\r\n \"params\": %s\r\n}"%(method,args_str)
+    headers = {
+        'content-type': "text/plain",
+        'authorization': "Basic %s"%(basestr),
+        'cache-control': "no-cache",
+        }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
 def btc_create_address():
-    btc_request("getnewaddress","")
-    pass
+    btc_request("getnewaddress",[])
 def get_account_list_btc_address():
-    btc_request("getaddressesbyaccount","")
-    pass
+    btc_request("getaddressesbyaccount",[""])
 def btc_collect_money(Address):
     pass
