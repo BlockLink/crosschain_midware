a12f94356935256a18fc14c982c5f022daeaedc3
zhangzhentao <28299209@qq.com>
2017-07-11 16:19:28 +0800

    创建索引，插入归账地址

--- mgmt/init_db.js
+++ mgmt/init_db.js
@@ -15,3 +15,37 @@ db.createCollection("b_cash_sweep");
 db.createCollection("b_cash_sweep_plan_detail")
 
 db.b_chain_account.ensureIndex({"chainId":1, "address": 1}, {"unique":true});
+db.s_user.ensureIndex({'email': 1}, {"unique":true});
+db.s_user.ensureIndex({'username': 1}, {"unique":true});
+db.b_block.ensureIndex({'chainId': 1});
+db.b_block.ensureIndex({'blockHash': 1});
+db.b_raw_transaction.ensureIndex({'chainId': 1});
+db.b_raw_transaction.ensureIndex({'trxId': 1});
+db.b_raw_transaction_input.ensureIndex({'rawTransactionid': 1});
+db.b_raw_transaction_input.ensureIndex({'address': 1});
+db.b_raw_transaction_output.ensureIndex({'rawTransactionid': 1});
+db.b_raw_transaction_output.ensureIndex({'address': 1});
+db.b_chain_account.ensureIndex({'name': 1});
+db.b_chain_account.ensureIndex({'address': 1});
+db.b_chain_account.ensureIndex({'chainId': 1});
+db.b_chain_account.ensureIndex({'creatorUserId': 1});
+db.b_chain_account.ensureIndex({'chainId': 1, 'address': 1}, {'unique': true});
+db.b_deposit_transaction.ensureIndex({'chainId': 1});
+db.b_deposit_transaction.ensureIndex({'fromAddress': 1});
+db.b_withdraw_transaction.ensureIndex({'chainId': 1});
+db.b_withdraw_transaction.ensureIndex({'toAddress': 1});
+db.b_config.ensureIndex({'key': 1}, {'unique': true});
+
+db.b_config.insert({
+    'key': 'cash_sweep_address',
+    'value': [
+        {
+            'chainId': 'eth',
+            'address': '0x1234'
+        },
+        {
+            'chainId': 'btc',
+            'address': 'adf892fg'
+        }
+    ]
+})
