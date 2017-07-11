9e8ae2f95b761e18436b6f151bc56c292a1c3850
tedmosby <530623363@qq.com>
2017-07-11 16:45:19 +0800

    btc_client_add

--- utils/btc_utils.py
+++ utils/btc_utils.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 import requests
 from base64 import encodestring
-
+import json
 def btc_request(method,args):
     url = "http://127.0.0.1:60011/"
     user = 'a'
     passwd = 'b'
     basestr = encodestring('%s:%s' % (user,passwd))[:-1]
-    args_str = ''.join(['[', ','.join(['"' + str(arg) + '"' for arg in args]), ']'])
-    payload =  "{\r\n \"id\": 1,\r\n \"method\": \"%s\",\r\n \"params\": %s\r\n}"%(method,args_str)
+    '''x = []
+    for arg in args:
+        x.append(arg)'''
+    args_j = json.dumps(args)
+    payload =  "{\r\n \"id\": 1,\r\n \"method\": \"%s\",\r\n \"params\": %s\r\n}"%(method,args_j)
     headers = {
         'content-type': "text/plain",
         'authorization': "Basic %s"%(basestr),
@@ -16,13 +19,11 @@ def btc_request(method,args):
         }
     response = requests.request("POST", url, data=payload, headers=headers)
     rep = response.json()
-    print(rep)
     return rep
 def btc_create_address():
-    btc_request("getnewaddress",["datest"])
+    print btc_request("getnewaddress",["1"])
 def get_account_list_btc_address():
     btc_request("getaddressesbyaccount",["datest"])
-
 def btc_collect_money(Address):
     rep = btc_request("getbalance",[""])
     balance = rep["result"]
