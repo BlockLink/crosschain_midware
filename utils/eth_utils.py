9cbff6aa5b07a7290b2d010b310e19f84f617400
zhangzhentao <28299209@qq.com>
2017-07-14 17:43:32 +0800

    只对Linux备份私钥

--- utils/eth_utils.py
+++ utils/eth_utils.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-#from __future__ import print_function
+# from __future__ import print_function
 import requests
 import json
 from config import config
-from datetime import datetime
+import sys
 import shutil
 import os
 from math import pow
@@ -14,8 +14,9 @@ import traceback
 
 temp_config = config["Sunny"]
 
+
 def eth_request(method, args):
-    url = "http://%s:%s/rpc"%(temp_config.ETH_URL,temp_config.ETH_PORT)
+    url = "http://%s:%s/rpc" % (temp_config.ETH_URL, temp_config.ETH_PORT)
     request_template = '''{"jsonrpc":"2.0","id":"1","method":"%s","params":%s}'''
     args_str = json.dumps(args)
     data_to_send = request_template % (method, args_str)
@@ -31,16 +32,19 @@ def eth_request(method, args):
 
 
 def eth_backup():
+    # 只对Linux平台进行备份
+    if sys.platform != 'linux2':
+        return True
     backup_path = '/var/backup_keystore/'
     if not os.path.exists(backup_path):
         os.makedirs(backup_path)
-    ret = eth_request("admin_datadir",[])
+    ret = eth_request("admin_datadir", [])
     json_data = json.loads(ret)
     if json_data.get("result") is None:
         return False
     source_path = json_data["result"]
 
-    shutil.copytree(source_path,backup_path)
+    shutil.copytree(source_path, backup_path)
     return True
 
 
@@ -51,22 +55,20 @@ def eth_create_address():
     result = eth_request("personal_newAccount", [temp_config.ETH_SECRET_KEY])
     json_result = json.loads(result)
     if json_result.has_key("result"):
-
         address = json_result["result"]
         return address
     return address
 
 
 def eth_get_base_balance(address):
-
-    result = eth_request("eth_getBalance", [address,"latest"])
+    result = eth_request("eth_getBalance", [address, "latest"])
     print result
     json_result = json.loads(result)
     if json_result.get("result") is None:
         return 0
-    amount = long(json_result["result"],16)
-    return float(amount/pow(10,18))
-    #return
+    amount = long(json_result["result"], 16)
+    return float(amount / pow(10, 18))
+    # return
 
 
 def eth_get_no_precision_balance(address):
@@ -92,15 +94,13 @@ def get_account_list_from_wallet():
     return addressList
 
 
-
 def get_transaction_data(trx_id):
-    #print "\ntrx_id:",trx_id
-    ret = eth_request("eth_getTransactionByHash",[str(trx_id)])
-    #print ret
+    # print "\ntrx_id:",trx_id
+    ret = eth_request("eth_getTransactionByHash", [str(trx_id)])
+    # print ret
     json_data = json.loads(ret)
     if json_data.get("result") is None:
-        return None,None
-
+        return None, None
 
     while True:
         if not json_data["blockNumber"] is None:
@@ -112,10 +112,10 @@ def get_transaction_data(trx_id):
             return None, None
         time.sleep(1)
     resp_data = json_data.get("result")
-    ret = eth_request("eth_getTransactionReceipt",[str(trx_id)])
+    ret = eth_request("eth_getTransactionReceipt", [str(trx_id)])
     json_data = json.loads(ret)
     if json_data.get("result") is None:
-        return None,None
+        return None, None
 
     while True:
         if not json_data["blockNumber"] is None:
@@ -127,13 +127,13 @@ def get_transaction_data(trx_id):
             return None, None
         time.sleep(1)
     receipt_data = json_data.get("result")
-    return resp_data,receipt_data
+    return resp_data, receipt_data
 
 
-#创建转账交易 默认GasPrice 5Gwei
-def eth_send_transaction(from_address,to_address,value,gasPrice="0x1dcd6500",gas="0x76c0"):
+# 创建转账交易 默认GasPrice 5Gwei
+def eth_send_transaction(from_address, to_address, value, gasPrice="0x1dcd6500", gas="0x76c0"):
     ret = eth_request("eth_sendTransaction", [{"from": account, "to": cash_sweep_account,
-                                               "value": hex(int(value * pow(10,18))).replace('L', ''),
+                                               "value": hex(int(value * pow(10, 18))).replace('L', ''),
                                                "gas": "0x76c0", "gasPrice": "0x1dcd6500"}])
     json_data = json.loads(ret)
     if json_data.get("result") is None:
@@ -142,44 +142,48 @@ def eth_send_transaction(from_address,to_address,value,gasPrice="0x1dcd6500",gas
         return json_data.get("result")
 
 
-
-def eth_collect_money(cash_sweep_account,accountList):
+def eth_collect_money(cash_sweep_account, accountList):
     try:
         result_data = {}
         result_data["errdata"] = []
         result_data["data"] = []
 
-        #存储创建成功的交易单号
+        # 存储创建成功的交易单号
         for account in accountList:
             amount = eth_get_no_precision_balance(account)
-            if float(amount)/pow(10,18) > temp_config.ETH_Minimum:
-                #转账给目标账户
-                result = eth_request("personal_unlockAccount",[account,temp_config.ETH_SECRET_KEY,10000])
+            if float(amount) / pow(10, 18) > temp_config.ETH_Minimum:
+                # 转账给目标账户
+                result = eth_request("personal_unlockAccount", [account, temp_config.ETH_SECRET_KEY, 10000])
                 if json.loads(result).get("result") is None:
-                    result_data["errdata"].append({"from_addr":account,"to_addr":cash_sweep_account,"amount":float(amount)/pow(10,18),"error_reason":u"账户解锁失败"})
-                    #写入归账失败的列表
+                    result_data["errdata"].append(
+                        {"from_addr": account, "to_addr": cash_sweep_account, "amount": float(amount) / pow(10, 18),
+                         "error_reason": u"账户解锁失败"})
+                    # 写入归账失败的列表
                     continue
 
-                ret = eth_request("eth_sendTransaction",[{"from": account, "to": cash_sweep_account,
-                                                          "value": hex(int((amount - pow(10,16)))).replace('L',''),
-                                                          "gas": "0x76c0", "gasPrice": "0x1dcd6500"}])
+                ret = eth_request("eth_sendTransaction", [{"from": account, "to": cash_sweep_account,
+                                                           "value": hex(int((amount - pow(10, 16)))).replace('L', ''),
+                                                           "gas": "0x76c0", "gasPrice": "0x1dcd6500"}])
                 if json.loads(result).get("result") is None:
-                    result_data["errdata"].append({"from_addr": account,"to_addr":cash_sweep_account,"amount":float(amount)/pow(10,18), "error_reason": u"账户创建交易失败"})
-                    #写入归账失败的列表
+                    result_data["errdata"].append(
+                        {"from_addr": account, "to_addr": cash_sweep_account, "amount": float(amount) / pow(10, 18),
+                         "error_reason": u"账户创建交易失败"})
+                    # 写入归账失败的列表
                     continue
                 else:
-                    result_data["data"].append({"from_addr": account,"to_addr":cash_sweep_account,"amount":float(amount)/pow(10,18),"trx_id":json.loads(result).get("result")})
-                    #获取交易详情按笔计入details
-                    #写入归账成功返回
+                    result_data["data"].append(
+                        {"from_addr": account, "to_addr": cash_sweep_account, "amount": float(amount) / pow(10, 18),
+                         "trx_id": json.loads(result).get("result")})
+                    # 获取交易详情按笔计入details
+                    # 写入归账成功返回
         return result_data
     except Exception, ex:
         logger.info(traceback.format_exc())
         return None, ex.message
 
 
-
 def eth_get_collect_money(accountList):
-    #从钱包获取归账地址列表
+    # 从钱包获取归账地址列表
     result_data = dict()
     result_data["details"] = []
 
@@ -188,26 +192,23 @@ def eth_get_collect_money(accountList):
         amount = eth_get_base_balance(account)
         one_data = {}
         one_data["address"] = account
-        one_data["amount"] = float(amount)/pow(10,18)
+        one_data["amount"] = float(amount) / pow(10, 18)
         result_data["details"].append(one_data)
-        total_amount += float(amount)/pow(10,18)
-
+        total_amount += float(amount) / pow(10, 18)
 
     result_data["total_amount"] = total_amount
     return result_data
 
 
-
-
 if __name__ == '__main__':
-    #get_account_list_from_wallet()
-    #eth_create_address()
-    #get_account_list_from_db()
-    #eth_collect_money(2,"0x085aa94b764316d5e608335d13d926c6c6911e56")
+    # get_account_list_from_wallet()
+    # eth_create_address()
+    # get_account_list_from_db()
+    # eth_collect_money(2,"0x085aa94b764316d5e608335d13d926c6c6911e56")
     account = "0x085aa94b764316d5e608335d13d926c6c6911e56"
     cash_sweep_account = "0xaf5d9e0b647d775a2f951bc4b34b84f6a301f381"
 
     ret = eth_request("eth_sendTransaction", [{"from": account, "to": cash_sweep_account,
-                                               "value": hex(int((pow(10, 18)))).replace('L',''), "gas": "0x76c0","gasPrice": "0x1dcd6500"}])
+                                               "value": hex(int((pow(10, 18)))).replace('L', ''), "gas": "0x76c0",
+                                               "gasPrice": "0x1dcd6500"}])
     print ret
-
