664bdcbb69e5d843f8210976dae7af2652bd4c2b
zhangzhentao <28299209@qq.com>
2017-07-18 11:26:49 +0800

    修改部分接口实现符合文档
    增加查询单个地址的余额

--- utils/error_utils.py
+++ utils/error_utils.py
@@ -14,17 +14,20 @@ def mismatched_parameter_type(v, t):
 def invalid_trx_type(v):
     return error_response(u'invalid transaction type: %s.' % v, 40002)
 
-def invalid_chaind_type(v):
+def invalid_chainid_type(v):
     return error_response(u'invalid chainId type: %s (eth, btc).' % v, 40003)
 
 def mis_cash_sweep_config():
     return error_response(u'cash sweep address config not exists.', 40004)
 
-def mis_cash_sweep_address(v):
-    return error_response(u'%s cash sweep address not set.'% v, 40005)
+def mis_cash_sweep_address(address):
+    return error_response(u'[%s] cash sweep address not set.'% address, 40005)
 
-def eth_address_invaild(address):
-    return error_response(u'address %s not start with 0x '%address,40006)
+def invalid_deposit_address(address):
+    return error_response(u'address [%s] is not in deposit address list '%address, 40006)
+
+def invaild_eth_address(address):
+    return error_response(u'address [%s] not start with 0x '%address,40007)
 
 def unexcept_error(ex):
     return error_response('chain happend unexcept error %s'%ex,49999)
