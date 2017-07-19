271c081364d1f7e46d89965d120768fd4956876c
zhangzhentao <28299209@qq.com>
2017-07-19 14:20:04 +0800

    初始化数据库前，等待5秒，等数据库完全启动

--- docker_op.sh
+++ docker_op.sh
@@ -3,6 +3,8 @@
 docker-compose build
 docker-compose down
 docker-compose up -d chaindb
-docker exec -it chaincommonapi_chaindb_1 mongo mgmt/init_db.js
+docker cp mgmt/init_db.js chaincommonapi_chaindb_1:/
+sleep 5
+docker exec -it chaincommonapi_chaindb_1 mongo init_db.js
 docker-compose up -d chain_midware
 
