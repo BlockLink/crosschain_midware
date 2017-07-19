856abcc17ddd54a4432a5dada451a81f971d7f1b
zhangzhentao <28299209@qq.com>
2017-07-19 16:43:08 +0800

    logger要保留

--- service/__init__.py
+++ service/__init__.py
@@ -4,11 +4,14 @@ from gevent import monkey
 monkey.patch_all()
 
 import os
+from config import logger
 from flask import Flask
 from config import config
 from pymongo import MongoClient
 from flask_jsonrpc import JSONRPC
 
+
+logger.info('Start app...')
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'))
 jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
@@ -16,8 +19,7 @@ jsonrpc = JSONRPC(app, '/api', enable_web_browsable_api=True)
 app.config.from_object(config['Sunny'])   # 载入配置文件
 
 client = MongoClient(app.config['MONGO_HOST'], app.config['MONGO_PORT'])
-db_auth = client[app.config['MONGO_NAME']]
-db_auth.authenticate(app.config['MONGO_USER'], app.config['MONGO_PASS'])
+
 db = client[app.config['MONGO_NAME']]
 
 from service import apis
