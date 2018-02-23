949fd3a698e4f6ab8704b4fc7ba0b53db74a7974
realm520 <zhangzhentao@blocklinker.com>
2018-02-23 15:52:04 +0800

    Remove unused code.

--- btc_data_collector/collect_ltc_block.py
+++ btc_data_collector/collect_ltc_block.py
@@ -16,12 +16,7 @@ from collect_btc_block import BTCCoinTxCollecter
 class LTCCoinTxCollecter(BTCCoinTxCollecter):
     def __init__(self, db):
         super(LTCCoinTxCollecter, self).__init__(db)
-        self.db = db
         self.t_multisig_address = self.db.b_ltc_multisig_address
-        self.last_sync_block_num = 0
-        self.sync_start_per_round = 0
-        self.sync_end_per_round = 0
-        self.sync_limit_per_step = 10
         self.config = LTCCollectorConfig()
         conf = {"host": self.config.RPC_HOST, "port": self.config.RPC_PORT}
         self.wallet_api = WalletApi(self.config.ASSET_SYMBOL, conf)
